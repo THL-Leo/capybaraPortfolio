b2ac2be392f824c71a66de827b6d2a192b97934d
Leo Lee <leolee901115@gmail.com>
2025-08-09 22:16:50 -0700

    aug 9 commit, set up sample sqlite db, might migrate to supabase or neon soon

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
-from flask import Flask
-import time
+import sqlite3
+from flask import Flask, g
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_time():
-    return {'time': time.time()}
+DATABASE = './test.db'
+
+def get_db():
+    db = getattr(g, '__database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontex
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
